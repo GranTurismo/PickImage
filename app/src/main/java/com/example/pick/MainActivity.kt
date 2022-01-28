6c37587a94e66cc16ba039013171bb8d2df92d4d
GranTurismo <32337747+GranTurismo@users.noreply.github.com>
2022-01-28 15:25:21 +0400

    cameraVersion + permission

--- app/src/main/java/com/example/pick/MainActivity.kt
+++ app/src/main/java/com/example/pick/MainActivity.kt
@@ -1,28 +1,32 @@
 package com.example.pick
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
 import androidx.activity.result.ActivityResultCallback
 import androidx.activity.result.contract.ActivityResultContracts
 import com.example.pick.databinding.ActivityMainBinding
+import java.security.Permission
+import java.security.Permissions
 
 class MainActivity : AppCompatActivity()
 {
-    private lateinit var binding:ActivityMainBinding
-    val rr = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
-    if(it.resultCode== RESULT_OK)
-    {
-        var uri = it.data!!.data!!
-        var stream = contentResolver.openInputStream(uri)
-        var bmp = BitmapFactory.decodeStream(stream)
-
-        binding.image.setImageBitmap(bmp)
+    private lateinit var binding: ActivityMainBinding
+    val rr = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+        if (it.resultCode == RESULT_OK)
+        {
+            var bmp = (it.data!!.extras!!.get("data") as Bitmap)
+            binding.image.setImageBitmap(bmp)
+        }
     }
-}
+
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
@@ -30,9 +34,14 @@ class MainActivity : AppCompatActivity()
         setContentView(binding.root)
 
         binding.select.setOnClickListener {
-            var intent = Intent(Intent.ACTION_PICK)
-            intent.type="image/*"
-            rr.launch(intent)
+            if (checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED)
+            {
+                requestPermissions(arrayOf(Manifest.permission.CAMERA), 0)
+            } else
+            {
+                var intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                rr.launch(intent)
+            }
         }
     }
 }
